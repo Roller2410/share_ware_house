c9e677a99479729909e0e0e3a00645959ab733f0
roller2410 <roller2410@outlook.com>
2019-08-18 23:16:54 +0100

    add controller

---
+++ config/constants.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'duoSetting' => [
+        'akey' => 'THISISMYSUPERSECRETCUSTOMERKEYDONOTSHARE',
+        'ikey' => 'DITAZ6S3A3KJR1WJ9Q2I',
+        'skey' => '3jLBN1c7T7ZftSsUrNhNHVSAKVZRvkSRn6sPMrpf',
+        'host' => 'api-95dbf0ff.duosecurity.com',
+    ]
+];
